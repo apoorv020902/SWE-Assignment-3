57dcc63d70352ea87b3719b550e59694ca348f79
Gage McGurie <72943845+Gage-McGuire@users.noreply.github.com>
2024-04-25 23:15:54 -0500

    basically done
    cost-calc.py reads the item then the price from each line of the items text file. Then adds key, value pairs into a set which if item key is already present the value will get updated. The code them gets the sum for all values in the set. All this then gets written to the expense report text file.

--- cost-calc.py
+++ cost-calc.py
@@ -1,1 +1,27 @@
-print("working")
+items_file = open("items.txt", "r")
+expense_file = open("expense_report.txt", "w")
+
+items = {}
+
+for line in items_file:
+    item = line.split()[0]
+    price = int(line.split()[1])
+
+    if item in items.keys():
+        old_total = items.get(item)
+        new_total = old_total + price
+        items.update({item:new_total})
+    else:
+        items[item] = price
+
+total = sum(items.values())
+
+for key, value in items.items():
+    expense_file.write("%s: %d\n" % (key, value))
+
+expense_file.write("total: %d" % total)
+
+items_file.close()
+expense_file.close()
+
+    
